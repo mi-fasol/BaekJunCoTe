f3cdd1ebfb645243caeee4eb6748c2f44c195405
mi-fasol <100758764+mi-fasol@users.noreply.github.com>
2023-04-25 03:54:14 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
@@ -0,0 +1,11 @@
+c = int(input())
+
+for i in range(c):
+    num = list(map(int, input().split()))
+    avg = sum(num[1:])/num[0]
+    cnt = 0
+    for j in num[1:]:
+        if j > avg:
+            cnt += 1
+    rate = '{:.3f}'.format(round(cnt/num[0]*100, 3))
+    print(rate + "%")
