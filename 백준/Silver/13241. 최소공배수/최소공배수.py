147ce6ee5ea2776609e8d8e16489e2f117837b3a
mi-fasol <100758764+mi-fasol@users.noreply.github.com>
2023-05-30 16:01:45 +0900

    [Silver V] Title: 최소공배수, Time: 40 ms, Memory: 33376 KB -BaekjoonHub

--- 백준/Silver/13241. 최소공배수/최소공배수.py
+++ 백준/Silver/13241. 최소공배수/최소공배수.py
@@ -1,10 +1,5 @@
 import math
 import sys
 
-result = []
-
 a, b = map(int, sys.stdin.readline().split())
-result.append(a*b/math.gcd(a, b))
-
-for i in result:
-    print(round(i))
+print(a*b//math.gcd(a, b))
