f2871f63eb7688dce373cc9843ee012b1b2b353a
song miran <100758764+mi-fasol@users.noreply.github.com>
2024-03-10 17:09:44 +0900

    [Silver III] Title: 계단 오르기, Time: 124 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static Integer[] dp;
+    static int[] arr;
+    static int num;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        num = Integer.parseInt(br.readLine());
+
+        dp = new Integer[num + 1];
+        arr = new int[num + 1];
+
+        for(int i = 1; i <= num; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        dp[0] = arr[0];
+        dp[1] = arr[1];
+        
+        if(num >= 2) dp[2] = arr[1] + arr[2];
+
+        System.out.println(recur(num));
+
+    }
+
+    public static int recur(int n) {
+        if(dp[n] == null){
+            dp[n] =  Math.max(recur(n - 2), recur(n - 3) + arr[n - 1]) + arr[n];
+        }
+        return dp[n];
+    }
+}
