9e85ae73252478d7ac1e988309265c7f3b4beee5
song miran <minyoong39@naver.com>
2024-04-20 17:14:17 +0900

    [Gold IV] Title: 이모티콘, Time: 144 ms, Memory: 14468 KB -BaekjoonHub

---
+++ 백준/Gold/14226. 이모티콘/이모티콘.java
@@ -0,0 +1,30 @@
+import java.io.*;
+
+public class Main {
+    static int N;
+    static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+
+        dp = new int[N + 1];
+
+        int[] dp = new int[4001];
+
+        for (int i = 0; i <= 4000; i++)
+            dp[i] = i;
+
+        for (int i = 1; i <= 1000; i++) {
+            for (int j = 2; true; j++) {
+                if (i * j > 1000) break;
+                dp[i * j] = Math.min(dp[i] + j, dp[i * j]);
+            }
+            for (int j = 1000; j >= 2; j--)
+                dp[j] = Math.min(dp[j + 1] + 1, dp[j]);
+        }
+
+        System.out.println(dp[N]);
+    }
+}
