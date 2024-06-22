31e36e1869d257eb8858e275442b2f44ab7c6875
song miran <minyoong39@naver.com>
2024-06-22 20:06:57 +0900

    [Gold V] Title: 최소비용 구하기, Time: 556 ms, Memory: 51748 KB -BaekjoonHub

---
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M, res = Integer.MAX_VALUE, start, dest;
+    static List<List<int[]>> graph = new ArrayList<>();
+    static StringTokenizer st;
+    static int[] dist;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
+
+        dist = new int[N + 1];
+
+        for (int i = 0; i <= N; i++) {
+            graph.add(new ArrayList<>());
+            dist[i] = Integer.MAX_VALUE;
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            graph.get(a).add(new int[]{b, c});
+        }
+
+        st = new StringTokenizer(br.readLine());
+        start = Integer.parseInt(st.nextToken());
+        dest = Integer.parseInt(st.nextToken());
+
+        daikstra();
+
+        System.out.println(res);
+    }
+
+    public static void daikstra() {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
+        pq.offer(new int[]{start, 0});
+
+        dist[start] = 0;
+
+        while (!pq.isEmpty()) {
+            int[] cur = pq.poll();
+            int curNode = cur[0];
+            int curDist = cur[1];
+
+            if (dist[curNode] < curDist) continue;
+
+            for (int[] next : graph.get(curNode)) {
+                int nextNode = next[0];
+                int nextDist = curDist + next[1];
+
+                if(nextNode == dest) {
+                    res = Math.min(res, nextDist);
+                }
+
+                if (dist[nextNode] > nextDist) {
+                    dist[nextNode] = nextDist;
+                    pq.offer(new int[]{nextNode, nextDist});
+                }
+            }
+        }
+    }
+}
