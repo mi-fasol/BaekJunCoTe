c408566a189e37ab1f091dc72f136cb20f3b6f1d
song miran <minyoong39@naver.com>
2024-09-09 18:16:05 +0900

    [Gold III] Title: 줄 세우기, Time: 684 ms, Memory: 49384 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M;
+    static int[] rank;
+    static ArrayList<ArrayList<Integer>> students = new ArrayList<>();
+    static ArrayList<Integer> result = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        rank = new int[N+1];
+
+        for (int i = 0; i <= N; i++) {
+            students.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            students.get(a).add(b);
+        }
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 0; j < students.get(i).size(); j++) {
+                int next = students.get(i).get(j);
+                rank[next]++;
+            }
+        }
+        
+        topologicalSort();
+
+        br.close();
+
+    }
+    public static void topologicalSort() {
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 1; i <= N; i++) {
+            if (rank[i] == 0) {
+                q.offer(i);
+            }
+        }
+
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+            result.add(cur);
+
+            for (int i = 0; i < students.get(cur).size(); i++) {
+                int next = students.get(cur).get(i);
+                rank[next]--;
+
+                if (rank[next] == 0) {
+                    q.offer(next);
+                }
+            }
+        }
+
+        for (Integer integer : result) {
+            System.out.print(integer + " ");
+        }
+    }
+}
